f6172bd5ac699ead8b447e41dc9d372cf7168897
Findus23 <l.winkler23@me.com>
2014-12-09 16:55:05 +0100

    Titelseite korrigiert

--- zeichen.sh
+++ zeichen.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 echo "aus pdf:"
-echo $(pdftotext main.pdf -enc UTF-8 - | wc -c)
+echo $(pdftotext main.pdf -enc UTF-8 - | wc -m)
 echo ""
 echo "mit detex:"
 echo ""
 echo "hardware.tex"
-echo $(detex hardware.tex | wc -c)
+echo $(detex hardware.tex | wc -m)
 echo ""
 echo "software.tex"
-echo $(detex software.tex | wc -c)
+echo $(detex software.tex | wc -m)
 echo ""
 echo "einleitung.tex"
-echo $(detex einleitung.tex | wc -c)
+echo $(detex einleitung.tex | wc -m)
 echo ""
 echo "gesamt"
-echo $(detex einleitung.tex software.tex hardware.tex| wc -c)
+echo $(detex einleitung.tex software.tex hardware.tex| wc -m)
